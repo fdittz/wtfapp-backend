3fc2592de64683b5780b35233dd917a695f52c26
fdittz <fdittz@gmail.com>
2019-05-15 16:38:41 -0300

    finally logging in user

--- routes/quake.js
+++ routes/quake.js
@@ -8,15 +8,9 @@ var isUserAuthenticated  = require('../middleware/auth')
 router.post('/login', function(req, res, next) {
     UserService.login(req.body.nickname, req.body.secret).then(success => {
         console.log(success);
-        return res.status(200).json({
-			status: 200,
-			message: "error"
-		});
+        return res.status(200).send(req.body.nickname);
     }, error => {
-        return res.status(403).json({
-			status: 403,
-			message: "invalid login/pass"
-		});
+        return res.status(403).send("Invalid login/pass");
     });
 });
 module.exports = router;
