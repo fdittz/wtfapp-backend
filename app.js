386cda761cf8d1b82f3743eb89b2b9b442dd66cb
fdittz <fdittz@gmail.com>
2019-05-31 18:18:08 -0300

    https

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -10,6 +13,17 @@ var morgan  = require('morgan')
 
 var app = express();
 
+// Certificate
+const privateKey = fs.readFileSync('/home/qw/archive/tf.quadclub.com.br/privkey1.pem', 'utf8');
+const certificate = fs.readFileSync('/home/qw/archive/tf.quadclub.com.br/cert1.pem', 'utf8');
+const ca = fs.readFileSync('/home/qw/archive/tf.quadclub.com.br/chain1.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
@@ -19,7 +33,24 @@ app.use('/api/users', users);
 app.use('/api/quake', quake);
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.js'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
 app.use(morgan('combined'))
+
+// Starting both http & https servers
+const httpServer = http.createServer(function (req,res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+});
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
 module.exports = app;
