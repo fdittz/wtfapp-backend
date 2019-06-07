3658727f7c2c426fc7c78968b61f6499c428406e
fdittz <fdittz@gmail.com>
2019-06-07 19:38:31 -0300

    various improvements

--- dev.js
+++ dev.js
@@ -5,16 +5,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var serveStatic = require('serve-static');
 
 var users = require('./routes/users');
 var public = require('./routes/public')
 var quake = require('./routes/quake')
+var servers = require('./routes/servers')
+var matches = require('./routes/matches');
 var morgan  = require('morgan')
-var serve = serveStatic('logs');
+const compress = require('compression');
+var winston = require('./config/winston');
 
 var app = express();
-
+app.use(compress())
+app.use(morgan('combined', { stream: winston.stream }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
@@ -22,6 +25,9 @@ app.use(cookieParser())
 app.use('/api/public', public);
 app.use('/api/users', users);
 app.use('/api/quake', quake);
+app.use('/api/servers', servers);
+app.use('/api/matches', matches);
+
 app.use(express.static('dist'));
 app.get('/demos', function(req, res) {
     res.redirect('http://tf.quadclub.com.br:27520/');
